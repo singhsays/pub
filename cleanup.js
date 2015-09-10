79af2cd5ecb68e8912887b4f50078aa748cbcdb5
singhsays <singhsays@gmail.com>
2015-09-10 14:52:54 +1000

    fixed path and minified

--- cleanup.js
+++ cleanup.js
@@ -1,4 +1,4 @@
 // Insert Jquery
 var s=document.createElement('script');
-s.setAttribute('src','cleanup.min.css');
+s.setAttribute('src','//raw.githubusercontent.com/singhsays/pub/master/cleanup.min.css');
 document.getElementsByTagName('head')[0].appendChild(s);
