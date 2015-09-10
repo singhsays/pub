025007336efe5be6bb66bb478ef432e0d695a047
singhsays <singhsays@gmail.com>
2015-09-10 14:59:27 +1000

    Use rawgit

--- cleanup.js
+++ cleanup.js
@@ -2,5 +2,5 @@
 var s=document.createElement('link');
 s.setAttribute('rel', 'stylesheet');
 s.setAttribute('type', 'text/css');
-s.setAttribute('href','//raw.githubusercontent.com/singhsays/pub/master/cleanup.min.css');
+s.setAttribute('href','//cdn.rawgit.com/singhsays/pub/master/cleanup.min.css');
 document.getElementsByTagName('head')[0].appendChild(s);
