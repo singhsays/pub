fd2bd2fb9bbe6a8938dd9455f3b4a2c41ac3e6db
singhsays <singhsays@gmail.com>
2015-09-10 14:55:11 +1000

    fix stylesheet

--- cleanup.js
+++ cleanup.js
@@ -1,4 +1,6 @@
 // Insert Jquery
-var s=document.createElement('script');
-s.setAttribute('src','//raw.githubusercontent.com/singhsays/pub/master/cleanup.min.css');
+var s=document.createElement('link');
+s.setAttribute('rel', 'stylesheet');
+s.setAttribute('type', 'text/css');
+s.setAttribute('href','//raw.githubusercontent.com/singhsays/pub/master/cleanup.min.css');
 document.getElementsByTagName('head')[0].appendChild(s);
